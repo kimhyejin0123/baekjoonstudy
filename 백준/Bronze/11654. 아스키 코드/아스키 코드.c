271a6c7f933c3c8bd89e728eac88f570a897e405
Hyejin Kim <110371904+kimhyejin0123@users.noreply.github.com>
2023-03-29 11:38:06 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+
+int main() {
+    char x ;
+    
+    scanf("%s \n", &x); //x를 입력받는 함수  x를 char타입으로 입력을 받는다. 
+    
+    printf("%d \n", x); //출력한다. 
+    
+    
+    return 0;
+}
