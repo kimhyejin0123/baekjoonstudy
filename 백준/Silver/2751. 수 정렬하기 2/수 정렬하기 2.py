9e060814838cd67b38e108594cb61012e9e7473b
Hyejin Kim <110371904+kimhyejin0123@users.noreply.github.com>
2023-04-09 22:11:33 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1496 ms, Memory: 84924 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,10 +1,14 @@
 import sys
 
-N=int(sys.stdin.readline().strip()) # 변경부분
+N=int(sys.stdin.readline().strip()) 
+
 n_list=[]
+
 for i in range(0,N):
-    a=int(sys.stdin.readline().strip()) #변경부분
+    a=int(sys.stdin.readline().strip()) 
     n_list.append(a)
+    
 a=sorted(n_list)
+
 for i in a:
     print(i)
