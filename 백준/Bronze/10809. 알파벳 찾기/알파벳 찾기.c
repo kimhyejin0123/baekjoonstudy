ef789543236a1af86944395cb093d80c251bc7ba
Hyejin Kim <110371904+kimhyejin0123@users.noreply.github.com>
2023-06-25 19:30:09 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.c
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-int main(void)
-{
+int main(){
     char word[100] = { 0 };
     scanf("%s", word);
+    
     for (int i = 97; i <= 122; i++) {
         int j = 0;
         while (word[j] != 0) {
