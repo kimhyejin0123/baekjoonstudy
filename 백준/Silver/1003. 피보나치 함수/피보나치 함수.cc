764713c63a6a8991d1e066ca44e945e7fa8e2c3a
hyeJin <110371904+kimhyejin0123@users.noreply.github.com>
2024-10-30 23:14:29 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -0,0 +1,27 @@
+//
+// Created by hjin0 on 2024-10-30.
+//
+#include <iostream>
+
+using namespace std;
+
+int dp[41] = { 0, 1, 1};
+
+int main()
+{
+    int T, N;
+
+    cin >> T;
+
+    for (int i = 3; i < 41; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    for (int i = 0; i < T; i++) {
+        cin >> N;
+        if (N == 0) cout << 1 << ' ' << 0 << '\n';
+        else if (N == 1) cout << 0 << ' ' << 1 << '\n';
+        else cout << dp[N - 1] << ' ' << dp[N] << '\n';
+    }
+    return 0;
+}
